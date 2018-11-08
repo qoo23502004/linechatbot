c1835c4cfd7d5b7c5133ad48090b495efda33526
qoo23502004 <qoo23502004@yahoo.com.tw>
2018-11-08 11:53:29 +0800

    add some code

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token, message)
     if event.message.text=="WHAT":
         string = sayhi()
-        message = TextSendMessage(text=string)
+        message = TextSendMessage(text=sayhi())
         line_bot_api.reply_message(event.reply_token, message)
 
 
