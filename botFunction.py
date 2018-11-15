3d9c9ebc54bec97936cfe98656969183170b4441
qoo23502004 <johnson18205@gmail.com>
2018-11-15 15:34:37 +0800

    Update and rename sayhi.py to botFunction.py

--- botFunction.py
+++ botFunction.py
@@ -1,13 +1,12 @@
 import requests
-def say():
+
+def checkState():
     url = "https://decapi.me/twitch/uptime?channel=vv0z1"
-    content = requests.get(url).text
-    
+    content = requests.get(url).text    
     return content
     
 
 
 if __name__ == "__main__":
-    a=say()
-    print(a)
-
+    
+    print(" ")
