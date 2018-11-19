47a76e6f5cf0e5e069a9439bfa25dcc0e2cf2b8e
qoo23502004 <qoo23502004@yahoo.com.tw>
2018-11-19 16:03:46 +0800

    change streamer url

--- botFunction.py
+++ botFunction.py
@@ -3,7 +3,7 @@ import re
 from bs4 import BeautifulSoup
 
 def checkState():
-    url = "https://decapi.me/twitch/uptime?channel=vv0z1"
+    url = "https://decapi.me/twitch/uptime?channel=ramiii"
     content = requests.get(url).text
     
     return content
