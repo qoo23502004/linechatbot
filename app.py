49bc4fafd11de6b4bf49616ae10152144d63fb0b
qoo23502004 <qoo23502004@yahoo.com.tw>
2018-11-20 15:06:26 +0800

    f

--- app.py
+++ app.py
@@ -45,8 +45,8 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token, message)
 
     t=time.strftime("%H:%M:%S", time.localtime())
-    if t=="15:07:00":       
-        message=TextSendMessage(text="現在時間，下午3點05分")
+    if t=="15:08:00":       
+        message=TextSendMessage(text="現在時間，下午3點08分")
         line_bot_api.push_message(push_token, message)
     
     keyword=event.message.text
