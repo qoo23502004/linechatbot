a28d1b9e797016d987fc55a643eaad6766ef9eaf
qoo23502004 <qoo23502004@yahoo.com.tw>
2018-12-21 09:26:13 +0800

    test

--- app.py
+++ app.py
@@ -93,10 +93,10 @@ def handle_message(event):
 
     ans=event.message.text
     ansCut=ans.split(' ')
-    if ansCut[0]=="!機器人" and len(ansCut)>=2:
+    if ansCut[0]=="!機器人" and len(ansCut)>=2 and event.source.user_id in adminID:
         for i in range(1,len(ansCut)):
             pushAns=pushAns+ansCut[i]+" "
-        profile = line_bot_api.get_group_member_profile(event.source.group_id,event.source.user_id)      
+        #profile = line_bot_api.get_group_member_profile(event.source.group_id,event.source.user_id)      
         line_bot_api.push_message("Cffc4e3c256a638f9f11e89c1171a9f4b", TextSendMessage(text=pushAns))
         #message = TextSendMessage(text=event.source.user_id)
         #line_bot_api.reply_message(event.reply_token, message) 
