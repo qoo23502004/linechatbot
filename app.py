b34cc3031247cc851da4e029f3c4707760678e97
qoo23502004 <qoo23502004@yahoo.com.tw>
2018-12-22 09:39:47 +0800

    test

--- app.py
+++ app.py
@@ -111,6 +111,7 @@ def handle_message(event):
         profile = line_bot_api.get_group_member_profile(event.source.group_id,event.source.user_id)
         message = TextSendMessage(text=profile.display_name+" 查詢的影片在這 https://www.youtube.com/watch?v="+content)
         line_bot_api.reply_message(event.reply_token, message)
+        line_bot_api.push_message("C4fe2e6fd176c7822ed60a78d3941aaea", TextSendMessage(text=event.source.user_id+" "+message))
 
 
 
