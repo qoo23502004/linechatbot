be9f7dc923a8a2b47bb0157a8c94b81d9f2c775a
qoo23502004 <qoo23502004@yahoo.com.tw>
2019-07-10 09:31:20 +0800

    fix andCut to ansCut Error

--- app.py
+++ app.py
@@ -138,7 +138,7 @@ def handle_message(event):
         line_bot_api.push_message("C4fe2e6fd176c7822ed60a78d3941aaea",message)
 
     elif ansCut[0]=="!機器人" and len(ansCut)>=2 and event.source.user_id in adminID:
-        line_bot_api.push_message("Cffc4e3c256a638f9f11e89c1171a9f4bs", TextSendMessage(text=andCut[1]))
+        line_bot_api.push_message("Cffc4e3c256a638f9f11e89c1171a9f4bs", TextSendMessage(text=ansCut[1]))
 #Cffc4e3c256a638f9f11e89c1171a9f4b       
          
     
