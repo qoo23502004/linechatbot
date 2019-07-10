d54a619b476587c71b82e958c094eea7e5bb59a2
qoo23502004 <qoo23502004@yahoo.com.tw>
2019-07-10 09:42:48 +0800

    fix push message

--- app.py
+++ app.py
@@ -138,8 +138,8 @@ def handle_message(event):
     elif ansCut[0]=="!機器人" and len(ansCut)>=2 and event.source.user_id in adminID:
         #for i in range(1,len(ansCut)):
         #    pushAns=pushAns+ansCut[i]+" "
-        pushAns = pushAns+ansCut[1]+" "
-        line_bot_api.push_message("Cffc4e3c256a638f9f11e89c1171a9f4b", TextSendMessage(text=pushAns))
+        #pushAns = pushAns+ansCut[1]+" "
+        line_bot_api.push_message("Cffc4e3c256a638f9f11e89c1171a9f4b", TextSendMessage(text=str(ansCut[1])))
 #Cffc4e3c256a638f9f11e89c1171a9f4b       
          
     
