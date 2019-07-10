9982856cb48d862f105def128ea25b3aaab87c14
qoo23502004 <qoo23502004@yahoo.com.tw>
2019-07-10 09:05:46 +0800

    test Fix 6

--- app.py
+++ app.py
@@ -129,14 +129,15 @@ def handle_message(event):
         message = TextSendMessage(text="測試權限成功")
         line_bot_api.reply_message(event.reply_token, message)
 
-    
-
-    ans=event.message.text
-    ansCut=ans.split(' ')
-    if ansCut[0]=="!機器人" and len(ansCut)>=2 and event.source.user_id in adminID:
-        for i in range(1,len(ansCut)):
-            pushAns=pushAns+ansCut[i]+" "    
-        line_bot_api.push_message("Cffc4e3c256a638f9f11e89c1171a9f4b", TextSendMessage(text=pushAns))
+    elif event.message.text=="!push":
+        message = TextSendMessage(text="push test")
+        line_bot_api.push_message("C4fe2e6fd176c7822ed60a78d3941aaea",message)
+#    ans=event.message.text
+#    ansCut=ans.split(' ')
+#    if ansCut[0]=="!機器人" and len(ansCut)>=2 and event.source.user_id in adminID:
+#        for i in range(1,len(ansCut)):
+#            pushAns=pushAns+ansCut[i]+" "    
+#        line_bot_api.push_message("Cffc4e3c256a638f9f11e89c1171a9f4b", TextSendMessage(text=pushAns))
        
          
     
