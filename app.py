c01064e457d1ee01b60777b888f109900a0d61f3
qoo23502004 <qoo23502004@yahoo.com.tw>
2019-07-10 09:17:11 +0800

    test Fix 8

--- app.py
+++ app.py
@@ -136,11 +136,11 @@ def handle_message(event):
         line_bot_api.push_message("C4fe2e6fd176c7822ed60a78d3941aaea",message)
 
     elif ansCut[0]=="!機器人" and len(ansCut)>=2 and event.source.user_id in adminID:
-        for i in range(1,len(ansCut)):
-            pushAns=pushAns+ansCut[i]+" "
+        #for i in range(1,len(ansCut)):
+        #    pushAns=pushAns+ansCut[i]+" "
     
-        line_bot_api.push_message("Cffc4e3c256a638f9f11e89c1171a9f4b", TextSendMessage(text=pushAns))
-       
+        line_bot_api.push_message("C4fe2e6fd176c7822ed60a78d3941aaea", TextSendMessage(text="test...push message"))
+#Cffc4e3c256a638f9f11e89c1171a9f4b       
          
     
 #    keyword=event.message.text
